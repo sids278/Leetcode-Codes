a806f65aaadab69865ec76c5c740b3efdf1617e3
sids278 <94206506+sids278@users.noreply.github.com>
2023-04-26 12:25:57 +0530

    Time: 3 ms (56.14%), Space: 5.9 MB (63.10%) - LeetHub

---
+++ 0258-add-digits/0258-add-digits.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int addDigits(int num) {
+       int ans=0;
+       while(num){
+           ans+=num%10;
+           num=num/10;
+       }
+       if(ans>=10){return addDigits(ans);}
+       return ans;
+       
+    }
+};
