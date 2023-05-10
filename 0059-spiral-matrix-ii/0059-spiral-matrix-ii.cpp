bb6d39e988937398ff6ebf2e0d62ea3c7794b3b9
sids278 <94206506+sids278@users.noreply.github.com>
2023-05-10 23:34:13 +0530

    Time: 4 ms (33.05%), Space: 6.7 MB (23.20%) - LeetHub

---
+++ 0059-spiral-matrix-ii/0059-spiral-matrix-ii.cpp
@@ -0,0 +1,44 @@
+class Solution {
+public:
+    vector<vector<int>> generateMatrix(int n) {
+      vector<vector<int>>matrix(n,vector<int>(n,0));
+        if (n == 0) {
+            return matrix;
+        }
+        
+        // Normal Case
+        
+        int rowStart = 0;
+        int rowEnd = n-1;
+        int colStart = 0;
+        int colEnd = n-1;
+        int num = 1; //change
+        
+        while (rowStart <= rowEnd && colStart <= colEnd) {
+            for (int i = colStart; i <= colEnd; i ++) {
+                matrix[rowStart][i] = num ++; //change
+            }
+            rowStart ++;
+            
+            for (int i = rowStart; i <= rowEnd; i ++) {
+                matrix[i][colEnd] = num ++; //change
+            }
+            colEnd --;
+            
+            for (int i = colEnd; i >= colStart; i --) {
+                if (rowStart <= rowEnd)
+                    matrix[rowEnd][i] = num ++; //change
+            }
+            rowEnd --;
+            
+            for (int i = rowEnd; i >= rowStart; i --) {
+                if (colStart <= colEnd)
+                    matrix[i][colStart] = num ++; //change
+            }
+            colStart ++;
+        }
+        
+        return matrix;
+    }
+    
+};
