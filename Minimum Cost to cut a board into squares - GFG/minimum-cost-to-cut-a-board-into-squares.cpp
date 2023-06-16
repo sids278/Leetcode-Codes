9b08dbdee322c0f72e07f95f3e495d909a7cbb89
sids278 <94206506+sids278@users.noreply.github.com>
2023-06-16 13:35:11 +0530

    Added solution - LeetHub

---
+++ Minimum Cost to cut a board into squares - GFG/minimum-cost-to-cut-a-board-into-squares.cpp
@@ -0,0 +1,64 @@
+//{ Driver Code Starts
+/* Driver program to test above function */
+
+#include<bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+//User function Template for C++
+
+class Solution {
+public:
+    int minimumCostOfBreaking(vector<int> X, vector<int> Y, int M, int N){
+        //Write your code here
+    
+        int ans = 0;
+        
+        sort(X.begin(), X.end(), greater<int>());
+        sort(Y.begin(), Y.end(), greater<int>());
+        
+        int i=0, j=0;
+        
+        while(i<M-1 and j<N-1)
+        {
+            if(X[i] >= Y[j])
+            ans += (j+1)*X[i++];
+            else
+            ans += (i+1)*Y[j++];
+        }
+        
+        while(i<M-1)
+        ans += (j+1)*X[i++];
+        
+        while(j<N-1)
+        ans += (i+1)*Y[j++];
+        
+        return ans;
+    }
+};
+
+
+//{ Driver Code Starts.
+int main()
+{
+	int t;
+	cin>>t;
+	while(t--)
+	{
+	    int m, n;
+	    cin >> m >> n;
+	    vector<int>X(m - 1), Y(n - 1);
+	    for(int i = 0; i < m - 1; i++){
+	        cin >> X[i];
+	    }
+	    for(int i = 0; i < n - 1; i++){
+	        cin >> Y[i];
+	    }
+	    Solution ob;
+	    int ans = ob.minimumCostOfBreaking(X, Y, m, n);
+	    cout << ans<<endl;
+	}
+	return 0;
+}
+
+// } Driver Code Ends
