375c87053674fe6a9733857214e7d7a3a81f797e
sids278 <94206506+sids278@users.noreply.github.com>
2023-06-26 00:31:35 +0530

    Time: 4 ms (80.28%), Space: 8.2 MB (78.87%) - LeetHub

---
+++ 1093-statistics-from-a-large-sample/1093-statistics-from-a-large-sample.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    vector<double> sampleStats(vector<int>& count) {
+        int n = 0, moda_count = 0, moda, mini = INT_MAX, maxi = 0;
+    long sum = 0;
+    
+    for(int i = 0; i != 256; i++)
+      if(count[i]){
+        n += count[i];
+        if(moda_count < count[i]) moda_count = count[i], moda = i;
+        mini = min(mini, i);
+        maxi = max(maxi, i);
+        sum += long(count[i])*i;
+      }
+    
+    int lim = (n+1)>>1, i = 0; 
+    while(lim > count[i]) lim -= count[i++];
+    double midi = i;
+    if(lim == count[i] && n%2 == 0){
+      while(count[++i] == 0);
+      midi = (midi + i)/2;
+    }
+       
+    return {double(mini), double(maxi), double(sum) / n, midi, double(moda)};
+    }
+};
