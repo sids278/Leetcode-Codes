89b365d18c89ec6ad80729904223018df7510f32
sids278 <94206506+sids278@users.noreply.github.com>
2023-07-13 07:13:52 +0530

    Time: 26 ms (64.01%), Space: 13.7 MB (39.67%) - LeetHub

--- 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.cpp
+++ 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.cpp
@@ -1,41 +1,58 @@
 class Solution {
 public:
-    int findCheapestPrice(int n, vector<vector<int>>& flights, int src, int dst, int k) {
-         vector<vector<pair<int, int>>> graph(n);
-        for(auto e : flights) {
-            graph[e[0]].push_back({e[1], e[2]});
+    int findCheapestPrice(int n, vector<vector<int>>& flights, int src, int dst, int k) 
+        {
+        // Create the adjacency list to depict airports and flights in
+        // the form of a graph.
+        vector<pair<int, int>> adj[n];
+        for (auto it : flights)
+        {
+            adj[it[0]].push_back({it[1], it[2]});
         }
-        priority_queue<vector<int>, vector<vector<int>>, greater<vector<int>>> pq;
-        // {dist_from_src_node, node, number_of_stops_from_src_node}
-        pq.push({0, src, 0});
+
+        // Create a queue which stores the node and their distances from the
+        // source in the form of {stops, {node, dist}} with ‘stops’ indicating 
+        // the no. of nodes between src and current node.
+        queue<pair<int, pair<int, int>>> q;
         
-        vector<int> stops(n, INT_MAX);
-        // number of stops to reach indexth node with least possible price from src node
-        // as it will be calculated once the pq.top() equals to indexth node
-        
-        // pq.top() will always store least cost among the pq elements so if already stop is calculated
-        // and if that is greater than the cstop that means already we have a path with cheaper cost
-        // as well as with less or equal number of stops
-        
-        while(!pq.empty()) {
-            auto temp=pq.top();
-            int cdist=temp[0];
-            int cnode=temp[1];
-            int cstop=temp[2];
-            pq.pop();
-            
-            if(cstop>stops[cnode] || cstop>k+1)
+        q.push({0, {src, 0}});
+
+        // Distance array to store the updated distances from the source.
+        vector<int> dist(n, 1e9);
+        dist[src] = 0;
+
+        // Iterate through the graph using a queue like in Dijkstra with 
+        // popping out the element with min stops first.
+        while (!q.empty())
+        {
+            auto it = q.front();
+            q.pop();
+            int stops = it.first;
+            int node = it.second.first;
+            int cost = it.second.second;
+             if(node==dst)continue;
+            // We stop the process as soon as the limit for the stops reaches.
+            if (stops > k)
                 continue;
-            
-            stops[cnode]=cstop;
-            if(cnode==dst) {
-                return cdist;
-            }
-            
-            for(auto a : graph[cnode]) {
-                pq.push({cdist+a.second, a.first, cstop+1});
+            for (auto iter : adj[node])
+            {
+                int adjNode = iter.first;
+                int edW = iter.second;
+               
+                // We only update the queue if the new calculated dist is
+                // less than the prev and the stops are also within limits.
+                if (cost + edW < dist[adjNode] && stops <= k)
+                {
+                    dist[adjNode] = cost + edW;
+                    q.push({stops + 1, {adjNode, cost + edW}});
+                }
             }
         }
-        return -1;
+        // If the destination node is unreachable return ‘-1’
+        // else return the calculated dist from src to dst.
+        if (dist[dst] == 1e9)
+            return -1;
+        return dist[dst];
     }
+    
 };
