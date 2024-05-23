ba6333a1c966576344385b2cf8027c48d3d6f24a
Siddharth Sharma <94206506+sids278@users.noreply.github.com>
2024-05-24 01:49:09 +0530

    Time: 14 ms (58.18%), Space: 19.5 MB (95.78%) - LeetHub

--- 0324-wiggle-sort-ii/0324-wiggle-sort-ii.cpp
+++ 0324-wiggle-sort-ii/0324-wiggle-sort-ii.cpp
@@ -1,15 +1,28 @@
 class Solution {
 public:
     void wiggleSort(vector<int>& nums) {
-        int n= nums.size();
-        priority_queue<int> q(nums.begin(), nums.end());
-        for(int i=1; i<n; i += 2){
-            nums[i] = q.top();
-            q.pop();
-        }
-        for(int i=0; i<n; i += 2){
-            nums[i] = q.top();
-            q.pop();
+        int n = nums.size();
+        if (n < 2) return;
+    
+        // Find a median.
+        auto midptr = nums.begin() + n / 2;
+        nth_element(nums.begin(), midptr, nums.end());
+        int mid = *midptr;
+        
+        int i = (n - 1) / 2 * 2, j = i, k = 1;
+        for (int c = 0; c < n; ++c) {
+            if (nums[j] < mid) {
+                swap(nums[i], nums[j]);
+                i -= 2;
+                j -= 2;
+                if (j < 0) j = n / 2 * 2 - 1;
+            } else if (nums[j] > mid) {
+                swap(nums[j], nums[k]);
+                k += 2;
+            } else {
+                j -= 2;
+                if (j < 0) j = n / 2 * 2 - 1;
+            }
         }
     }
 };
