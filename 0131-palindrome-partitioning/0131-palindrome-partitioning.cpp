5b1c8c3316c1b4f534c1f04c3bce9583b47ebc8e
Siddharth Sharma <94206506+sids278@users.noreply.github.com>
2024-12-18 21:47:09 +0530

    Time: 50 ms (68.37%), Space: 57.9 MB (62.51%) - LeetHub

--- 0131-palindrome-partitioning/0131-palindrome-partitioning.cpp
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.cpp
@@ -1,36 +1,36 @@
 class Solution {
 public:
-    void dfs(int i, string s, vector<vector<int>>& dp, vector<vector<string>>& v, vector<string>& temp) {
-        if (i == s.length()) {
+    void dfs(int i,string s,vector<vector<int>>&dp,vector<vector<string>>&v,vector<string>&temp){
+        if(i==s.length()){
             v.push_back(temp);
             return;
         }
-        for (int end = i; end < s.length(); end++) {
-            if (dp[i][end]) {
-                temp.push_back(s.substr(i, end - i + 1));
-                dfs(end + 1, s, dp, v, temp);
+        for(int end=i;end<s.length();end++){
+            if(dp[i][end]){
+                temp.push_back(s.substr(i,end-i+1));
+                dfs(end+1,s,dp,v,temp);
                 temp.pop_back();
             }
+            
         }
+        return ;
     }
-
     vector<vector<string>> partition(string s) {
-        int n = s.length();
-        vector<vector<int>> dp(n, vector<int>(n, 0));
-        
-        for (int i = 0; i < n; i++) dp[i][i] = 1;
-
-        for (int i = n - 1; i >= 0; i--) {
-            for (int j = i + 1; j < n; j++) {
-                if (s[i] == s[j] && (j - i == 1 || dp[i + 1][j - 1])) {
-                    dp[i][j] = 1;
-                }
+        int n=s.length();
+        vector<vector<int>>dp(n,vector<int>(n,0));
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                if(i==j)dp[i][j]=1;
             }
         }
-
-        vector<vector<string>> result;
-        vector<string> temp;
-        dfs(0, s, dp, result, temp);
+        for(int i=n-1;i>=0;i--){
+            for(int j=i+1;j<n;j++){
+                if(s[i]==s[j] && (j - i == 1 || dp[i + 1][j - 1]))dp[i][j]=1;
+            }
+        }
+        vector<vector<string>>result;
+        vector<string>temp;
+        dfs(0,s,dp,result,temp);
         return result;
     }
 };
