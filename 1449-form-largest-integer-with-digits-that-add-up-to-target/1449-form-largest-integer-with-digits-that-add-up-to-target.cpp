d0477b81dba0b07fbfdd9b0a4c5c854cb977ad98
Siddharth Sharma <94206506+sids278@users.noreply.github.com>
2026-06-14 15:50:41 +0530

    Time: 257 ms (40.49%), Space: 393.2 MB (31.9%) - LeetHub

---
+++ 1449-form-largest-integer-with-digits-that-add-up-to-target/1449-form-largest-integer-with-digits-that-add-up-to-target.cpp
@@ -0,0 +1,20 @@
+class Solution {
+    inline bool isGreaterThan(string &a, string &b) {
+        return a.size() != b.size() ? a.size() > b.size() : a > b;
+    }
+public:
+    string largestNumber(vector<int>& A, int T) {
+        int N = A.size();
+        vector<vector<string>> dp(N + 1, vector<string>(T + 1, "0"));
+        for (int i = 0; i <= N; ++i) dp[i][0] = "";
+        for (int i = 0; i < N; ++i) {
+            for (int c = 1; c <= T; ++c) {
+                dp[i + 1][c] = dp[i][c];
+                if (c < A[i] || dp[i + 1][c - A[i]] == "0") continue;
+                auto s = string(1, '1' + i) + dp[i + 1][c - A[i]];
+                if (isGreaterThan(s, dp[i + 1][c])) dp[i + 1][c] = s;
+            }
+        }
+        return dp[N][T];
+    }
+};
